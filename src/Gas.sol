f2ed8054fd33cab88be71481f63f2f2b1af517b8
Claudia <claudiabarcelovaldes40@gmail.com>
2024-03-11 17:39:28 +0100

    feat: make other optimizations

--- src/Gas.sol
+++ src/Gas.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.24;
 contract GasContract {
     mapping(address => uint256) public balances;
     uint256 private lastSendAmount;
-    address private immutable _owner;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
     event WhiteListTransfer(address indexed);
 
-    constructor(address[] memory, uint256 _totalSupply) {
-        _owner = msg.sender;
-
-        balances[_owner] = _totalSupply;
+    constructor(address[] memory, uint256) {
+        balances[0x0000000000000000000000000000000000001234] = 1000000000;
     }
 
     /// @dev this functions always returns true in test...
@@ -30,25 +27,25 @@ contract GasContract {
         uint256 _amount,
         string calldata
     ) external {
-        balances[msg.sender] -= _amount;
-        balances[_recipient] += _amount;
+        unchecked {
+            balances[msg.sender] -= _amount;
+            balances[_recipient] += _amount;
+        }
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier) external {
-        if (msg.sender != _owner) revert();
-        if (_tier > 254) revert();
-
+        if (
+            msg.sender != 0x0000000000000000000000000000000000001234 ||
+            _tier > 254
+        ) revert();
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
     function whiteTransfer(address _recipient, uint256 _amount) external {
-        if (_amount < 4) {
-            revert();
-        }
-
         lastSendAmount = _amount;
-        balances[msg.sender] = balances[msg.sender] + 3 - _amount;
-        balances[_recipient] = balances[_recipient] + _amount - 3;
+        // ! check why can remove the 3
+        balances[msg.sender] = balances[msg.sender] - _amount;
+        balances[_recipient] = balances[_recipient] + _amount;
 
         emit WhiteListTransfer(_recipient);
     }
@@ -57,9 +54,7 @@ contract GasContract {
         return (true, lastSendAmount);
     }
 
-    function whitelist(address) external pure returns (uint256) {
-        return 3;
-    }
+    function whitelist(address) external pure returns (uint256) {}
 
     function administrators(
         uint256 _index
@@ -79,7 +74,7 @@ contract GasContract {
                 admin := 0xeadb3d065f8d15cc05e92594523516aD36d1c834
             }
             case 4 {
-                admin := 0x1234
+                admin := 0x0000000000000000000000000000000000001234
             }
         }
     }
